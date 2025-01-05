447f4ae42d83402fa1f15e1426a7380de304cea0
Sandhiya Acharya <43362245+SandhiyaAcharya@users.noreply.github.com>
2025-01-05 14:04:47 +0530

    Add files via upload
    CSS and XGB Model Updated

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,jsonify
 import pandas as pd
 import joblib
+import xgboost as xgb
 
 app = Flask(__name__)
 
-# Load the dataset and model
+# Load the dataset, preprocessor and model
 data = pd.read_csv('Airbnb_India_dataset.csv') 
-# model = joblib.load('price_predictor.pkl')
-model = joblib.load('xgb_price_predictor.pkl')
+xgb_model = joblib.load('xgb_price_predictor.pkl')
+preprocessor = joblib.load('preprocessor.pkl')
 data.rename(columns={'pricing/rate/amount': 'price'}, inplace=True)
 
 @app.route('/')
 def home():
     # Fetch unique location values
     unique_locations = data['address'].dropna().unique()
-    unique_locations = sorted(unique_locations)  # Optional: Sort alphabetically
+    unique_locations = sorted(unique_locations)  # Sort alphabetically
     return render_template('index.html', locations=unique_locations)
 
 
@@ -34,23 +35,34 @@ def recommend():
 
     return render_template('recommendations.html', recommendations=output)
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
+    # Get the input data from the form
     room_type = request.form['room_type']
     number_of_guests = int(request.form['number_of_guests'])
     stars = float(request.form['stars'])
     address = request.form['address']
 
-    new_data = pd.DataFrame({
+    # Create a DataFrame to hold the input data (this is necessary for the preprocessor)
+    input_data = pd.DataFrame({
         'roomType': [room_type],
         'numberOfGuests': [number_of_guests],
         'stars': [stars],
         'address': [address]
     })
-    predicted_price = round(model.predict(new_data)[0])
-    return render_template('prediction.html', price=predicted_price)
 
+    # Preprocess the input data
+    input_preprocessed = preprocessor.transform(input_data)
+
+    # Convert to DMatrix for XGBoost prediction
+    dinput = xgb.DMatrix(input_preprocessed)
+
+    # Make the prediction
+    predicted_price = round(xgb_model.predict(dinput)[0])
+
+    # Render the prediction in the HTML template
+    return render_template('prediction.html', price=predicted_price)
 
 if __name__ == '__main__':
     app.run(debug=True)
-#testing
